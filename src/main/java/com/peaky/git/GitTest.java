9e63c621bf1d05709e54ece714b95cef8608faad
apple <2806391984@qq.com>
2022-10-28 11:16:03 +0800

    second commit

--- src/main/java/com/peaky/git/GitTest.java
+++ src/main/java/com/peaky/git/GitTest.java
@@ -3,5 +3,6 @@ package com.peaky.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello git");
+        System.out.println("second");
     }
 }
